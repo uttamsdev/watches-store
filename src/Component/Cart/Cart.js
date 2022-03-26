84abd2b195752d28cb6ddb94b0c70cfde20e9feb
UttamSaha <uttamsaha982@gmail.com>
2022-03-26 07:24:39 +0600

    cart button added

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -13,11 +13,13 @@ const Cart = (props) => {
         name = product.name;
     }
     return (
-        <div>
+        <div className="cart-items">
             <h2>Selected Watch: </h2>
             {
                 cart.map(product => <div className="selected-products" key={product.id}><img src={product.img}></img> {product.name} <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon> </div>)
             }
+            <button className="btn">CHOOSE 1 FOR ME</button> <br/>
+            <button className="btn">CHOOSE AGAIN</button>
         </div>
     )
 }
