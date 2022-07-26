a7888f3d20b084d5885463e4c9a9adb8c0e13afb
uttamsaha <uttamsaha982@gmail.com>
2022-07-26 11:12:26 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Shop from './Component/Shop/Shop';
 import Header from './Component/Header/Header';
-import QuestionAnswer from './Component/QuestionAnswer/QuestionAnswer';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Shop></Shop>
-      <QuestionAnswer></QuestionAnswer>
     </div>
   );
 }
